cbecf25d2dc196efa8fccc1ad9023536af99e8e8
Marcelo Sales <marcelo.d.sales@gmail.com>
2018-11-08 14:32:37 -0200

    update configuration

--- preference/dotnet/Controllers/ValuesController.cs
+++ preference/dotnet/Controllers/ValuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace dotnet.Controllers
 {
@@ -11,13 +13,21 @@ namespace dotnet.Controllers
     public class ValuesController : Controller
     {
         const string url = "http://recommendation:8080";
-        const string responseStringFormat = "preference => {0}\n";
-        // GET api/values
+        const string responseStringFormat = "preference from {0} => {1}\n";
+
+        private static readonly HttpClient client = new HttpClient();
+
         [HttpGet]
         public string Get()
         {
-            string hostname = Dns.GetHostName();
-            return String.Format(responseStringFormat, hostname);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Customer App");
+
+            var msg = client.GetStringAsync(url).Result;
+
+            string hostname = Dns.GetHostName();            
+            return String.Format(responseStringFormat, hostname, msg);
         }
     }
 }
